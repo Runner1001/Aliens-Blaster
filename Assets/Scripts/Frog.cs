621eb0b09d4f17565531ebad6d99f105af870936
Runner1001 <luca.villanini@gmail.com>
2023-02-21 20:04:05 +0100

    Added features to frog and added player
    Added SFX to the frog in 3D
    Added Player to the scene
    Changed the name of the scene in Level 1

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -7,32 +7,44 @@ public class Frog : MonoBehaviour
     [SerializeField] private float _jumpDelay = 3;
     [SerializeField] private Vector2 _jumpForce;
     [SerializeField] private Sprite _jumpSprite;
+    [SerializeField] private int _jumps = 2;
 
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
+    private AudioSource _audioSource;
     private Sprite _idleSprite;
+    private int _jumpsRemaining;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
+        _audioSource = GetComponent<AudioSource>();
 
         _idleSprite = _sr.sprite;
+        _jumpsRemaining = _jumps;
 
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
     }
 
     private void Jump()
     {
+        if(_jumpsRemaining <= 0)
+        {
+            _jumpForce *= new Vector2(-1, 1);
+            _jumpsRemaining = _jumps;
+        }
+
+        _jumpsRemaining--;
         _rb.AddForce(_jumpForce);
 
-        _jumpForce *= new Vector2(-1, 1);
-        _sr.flipX = !_sr.flipX;
+        _sr.flipX = _jumpForce.x > 0;
         _sr.sprite = _jumpSprite;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         _sr.sprite = _idleSprite;
+        _audioSource.Play();
     }
 }
