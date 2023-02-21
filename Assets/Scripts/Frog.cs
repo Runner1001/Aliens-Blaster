e7fb6a798475fd5373f513431541683303eeb71d
Runner1001 <luca.villanini@gmail.com>
2023-02-21 19:31:50 +0100

    Update Frog.cs
    Added Frog flips graphics

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -6,14 +6,18 @@ public class Frog : MonoBehaviour
 {
     [SerializeField] private float _jumpDelay = 3;
     [SerializeField] private Vector2 _jumpForce;
+    [SerializeField] private Sprite _jumpSprite;
 
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
+    private Sprite _idleSprite;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
+
+        _idleSprite = _sr.sprite;
 
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
     }
@@ -21,5 +25,14 @@ public class Frog : MonoBehaviour
     private void Jump()
     {
         _rb.AddForce(_jumpForce);
+
+        _jumpForce *= new Vector2(-1, 1);
+        _sr.flipX = !_sr.flipX;
+        _sr.sprite = _jumpSprite;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        _sr.sprite = _idleSprite;
     }
 }
