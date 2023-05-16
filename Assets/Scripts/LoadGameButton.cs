a8c2d830ae0c7790a3af0dc5c2f7f649935b9ac4
Runner1001 <luca.villanini@gmail.com>
2023-05-16 19:05:50 +0200

    Added support for multiple save games

--- Assets/Scripts/LoadGameButton.cs
+++ Assets/Scripts/LoadGameButton.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadGameButton : MonoBehaviour
 {
+    private string _gameName;
+
     void Start() => GetComponent<Button>().onClick.AddListener(LoadGame);
 
-    public void LoadGame() => GameManager.Instance.LoadGame();
+    public void LoadGame() => GameManager.Instance.LoadGame(_gameName);
+
+    public void SetGameName(string gameName)
+    {
+        _gameName = gameName;
+        GetComponentInChildren<TMP_Text>().SetText(_gameName);
+    }
 }
