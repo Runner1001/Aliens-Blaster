e5f3883fcf6a451d58d3e5f54272ce7fd4e33445
Runner1001 <luca.villanini@gmail.com>
2023-07-02 17:11:11 +0200

    Fixed duck for controller

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -28,7 +28,7 @@ public class PlayerAnimation : MonoBehaviour
     {
         _anim.SetBool("Jump", !_groundCheck.IsGrounded);
         _anim.SetBool("Move", _playerMovement.Horizontal != 0);
-        _anim.SetBool("Duck", _playerInput.Vertical < 0);
+        _anim.SetBool("Duck", _playerInput.Vertical < 0 && Mathf.Abs(_playerInput.Vertical) > Mathf.Abs(_playerMovement.Horizontal));
         IsDucking = _anim.GetBool("IsDucking");
         //_anim.SetFloat("HorizontalSpeed", Mathf.Abs(_playerMovement.Horizontal));
 
