29ee8ae556caf94953a9c97f82d4176bc90cf69c
Runner1001 <luca.villanini@gmail.com>
2025-07-07 22:03:10 +0200

    Added splines attack points

---
+++ Assets/Scripts/SplineAttackPoints.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineAttackPoints : MonoBehaviour
+{
+    [SerializeField] SplineContainer splineContainer;
+    [SerializeField] List<float> attackPoints;
+
+    void OnDrawGizmos()
+    {
+        foreach(var point in attackPoints)
+        {
+            Vector3 position = splineContainer.EvaluatePosition(point);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(position, 0.2f);
+        }
+    }
+}
